1a494a68a300ba4046d62be954e6602a4ef47f2a
diazguerraadrian-cmyk <diazguerraadrian@gmail.com>
2026-04-27 13:38:19 +0200

    ReviewControlador + Lista-reviews.html

--- src/main/java/com/example/restaurant/controlador/ReviewControlador.java
+++ src/main/java/com/example/restaurant/controlador/ReviewControlador.java
@@ -1,4 +1,22 @@
 package com.example.restaurant.controlador;
 
+import com.example.restaurant.repository.ReviewRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@AllArgsConstructor
 public class ReviewControlador {
+
+    //Inyectar el repositorio de reviews
+    private final ReviewRepository reviewRepository;
+
+    // getmapping reviews
+    @GetMapping("reviews")
+    public String reviews(Model model) {
+        model.addAttribute("reviews", reviewRepository.findAll());
+        return "reviews/Lista-reviews";
+    }
 }
