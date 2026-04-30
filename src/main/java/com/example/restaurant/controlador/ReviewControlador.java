8b685094ef551c7de1c6507d051b78c0a3e496fc
diazguerraadrian-cmyk <diazguerraadrian@gmail.com>
2026-04-30 13:46:35 +0200

    Acciones de ver editar y borrar en review list

--- src/main/java/com/example/restaurant/controlador/ReviewControlador.java
+++ src/main/java/com/example/restaurant/controlador/ReviewControlador.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @AllArgsConstructor
@@ -18,5 +20,19 @@ public class ReviewControlador {
     public String reviews(Model model) {
         model.addAttribute("reviews", reviewRepository.findAll());
         return "reviews/Lista-reviews";
+    }
+
+
+    @GetMapping("reviews/{id}")
+    public String review(Model model, @PathVariable Long id){
+        model.addAttribute("review", reviewRepository.findById(id).orElseThrow());
+        return "reviews/detalles-review";
+    }
+
+    @GetMapping("reviews/delete/{id}")
+    public String delete(@PathVariable Long id, RedirectAttributes redirectAttributes){
+        reviewRepository.deleteById(id);
+        redirectAttributes.addFlashAttribute("mensaje", "Review eliminada correctamente");
+        return "redirect:/reviews";
     }
 }
