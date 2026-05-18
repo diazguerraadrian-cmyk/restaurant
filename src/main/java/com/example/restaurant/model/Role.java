277f0aece40696a2361f67f58ef35d8ab99bb609
diazguerraadrian-cmyk <diazguerraadrian@gmail.com>
2026-05-18 11:51:20 +0200

    Creados Role y Usuario

---
+++ src/main/java/com/example/restaurant/model/Role.java
@@ -0,0 +1,6 @@
+package com.example.restaurant.model;
+
+public enum Role {
+    ROLE_USER,ROLE_ADMIN
+
+}
---
+++ src/main/java/com/example/restaurant/model/Usuario.java
@@ -0,0 +1,42 @@
+package com.example.restaurant.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "usuarios")
+public class Usuario implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+}
